6ed3702bd509a9cb50afbe864e9dbb6ada6d7749
Jacob <jacobs0408@yahoo.com>
2019-08-26 16:47:26 -0500

    yawn

--- sql/connection.js
+++ sql/connection.js
@@ -7,7 +7,7 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '35.184.48.70',
         user: 'root',
         password: 'password',
         database: 'admin'
